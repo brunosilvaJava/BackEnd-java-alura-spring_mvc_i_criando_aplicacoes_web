ef3d766e6cd14276d54fe0ddff863a25cb5031a4
Bruno Silva <bruno.silva890@etec.sp.gov.br>
2017-08-01 17:03:11 -0300

    Aula 07 - Atividade 02

--- CasaDoCodigo/WebContent/WEB-INF/views/produtos/listarProdutos.jsp
+++ CasaDoCodigo/WebContent/WEB-INF/views/produtos/listarProdutos.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 
 <!DOCTYPE html>
 <html>
@@ -9,7 +10,9 @@
 </head>
 <body>
 
-	<h1>Lista de Produtos</h1>.
+	<h1>Lista de Produtos</h1>
+	
+    <p> ${statusInclusaoLivro}</p>
     
     <table>
         <tr>
--- CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutoController.java
+++ CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutoController.java
@@ -1,14 +1,21 @@
 package br.com.casadocodigo.loja.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.bo.ProdutoBO;
 import br.com.casadocodigo.loja.enumerador.TipoLivro;
 import br.com.casadocodigo.loja.model.Produto;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("/produtos")
@@ -16,6 +23,13 @@ public class ProdutoController {
 	
 	@Autowired
 	private ProdutoBO produtoBO;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		
+		webDataBinder.addValidators(new ProdutoValidation());
+
+	}
 	
 	@RequestMapping(value="/initIncluirProduto", method=RequestMethod.GET)
 	public ModelAndView initIncluirProduto(){
@@ -28,11 +42,17 @@ public class ProdutoController {
 	}
 	
 	@RequestMapping(value="/gravarProduto", method=RequestMethod.POST)
-	public String gravarProduto(Produto produto){
+	public ModelAndView gravarProduto(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributtes){
+		
+		if(result.hasErrors()){
+	        return initIncluirProduto();
+	    }
 		
 		produtoBO.gravar(produto);
+		
+		redirectAttributtes.addFlashAttribute("statusInclusaoLivro", "Produto cadastrado com sucesso!");
 
-	    return "ok";
+	    return new ModelAndView("redirect:listarProdutos");
 	    
 	}
 
